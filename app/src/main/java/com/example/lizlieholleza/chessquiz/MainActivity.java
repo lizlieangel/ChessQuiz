cd303bd377b37306b9872f0d4ee24ba2e255fab0
Liz <lizlie.holleza@awsys-i.com>
2018-08-10 13:24:10 +0800

    add method for the first question

--- app/src/main/java/com/example/lizlieholleza/chessquiz/MainActivity.java
+++ app/src/main/java/com/example/lizlieholleza/chessquiz/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.lizlieholleza.chessquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void answerQuestionOne(View v) {
+        boolean selectAnswer = ((RadioButton) v).isChecked();
+        switch (v.getId()) {
+            case R.id.q_one_a:
+                if(selectAnswer) {
+                    Toast.makeText(this,"Your answer is correct! :)", Toast.LENGTH_SHORT ).show();
+                    score += 1;
+                }
+                break;
+            case R.id.q_one_b:
+                if(selectAnswer) {
+                    Toast.makeText(this, "You got it wrong. :/", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
